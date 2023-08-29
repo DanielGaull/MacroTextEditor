11d3482d7bff0b1843fec1297fe6a4c991e0c407
Daniel Gaull <danieljgaull@gmail.com>
2023-08-29 14:49:37 -0400

    Cleanup

--- TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/MainUiController.java
+++ TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/MainUiController.java
@@ -1,21 +1,12 @@
 package com.danieljgaull.texteditor.texteditor;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
-import javafx.stage.FileChooser;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Stream;
 
 public class MainUiController {
     @FXML
@@ -26,8 +17,6 @@ public class MainUiController {
     private Label statusText;
 
     private TextEditorController textEditorController;
-
-    private File currentLoadedFile;
 
     public void initialize() {
         // Only set the progress bar visible when stuff is loading
