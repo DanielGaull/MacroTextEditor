9abc91283dfe6b3c25488d71046f4b78210deb0b
Daniel Gaull <danieljgaull@gmail.com>
2023-08-31 13:02:33 -0400

    Added AST support for functions

--- TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/expressions/Ast.java
+++ TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/expressions/Ast.java
@@ -1,5 +1,9 @@
 package com.danieljgaull.texteditor.texteditor.expressions;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Ast {
 
     private AstTypes type;
@@ -14,13 +18,17 @@ public class Ast {
     private String stringValue;
     private double numberValue;
     private boolean boolValue;
+
+    // Used for variable names and function calls
     private String variableName;
+    // Currently only used for function args
+    private List<Ast> children;
 
     // Operator types
     private int op;
 
     private Ast(AstTypes type, Ast left, Ast right, Ast middle, String str, double num, boolean bool, String var,
-                int op) {
+                int op, List<Ast> children) {
         this.type = type;
         this.left = left;
         this.right = right;
@@ -30,28 +38,35 @@ public class Ast {
         this.boolValue = bool;
         this.variableName = var;
         this.op = op;
+        this.children = children;
     }
 
     public static Ast string(String value) {
-        return new Ast(AstTypes.StringLiteral, null, null, null, value, 0, false, null, -1);
+        return new Ast(AstTypes.StringLiteral, null, null, null, value, 0, false, null, -1, new ArrayList<>());
     }
     public static Ast number(double value) {
-        return new Ast(AstTypes.NumberLiteral, null, null, null, null, value, false, null, -1);
+        return new Ast(AstTypes.NumberLiteral, null, null, null, null, value, false, null, -1, new ArrayList<>());
     }
     public static Ast bool(boolean value) {
-        return new Ast(AstTypes.BooleanLiteral, null, null, null, null, 0, value, null, -1);
+        return new Ast(AstTypes.BooleanLiteral, null, null, null, null, 0, value, null, -1, new ArrayList<>());
     }
     public static Ast variable(String name) {
-        return new Ast(AstTypes.Variable, null, null, null, null, 0, false, name, -1);
+        return new Ast(AstTypes.Variable, null, null, null, null, 0, false, name, -1, new ArrayList<>());
+    }
+    public static Ast functionCall(String name, List<Ast> args) {
+        return new Ast(AstTypes.FunctionCall, null, null, null, null, 0, false, name, -1, args);
     }
     public static Ast unary(Ast single, UnaryOperators operator) {
-        return new Ast(AstTypes.UnaryOperation, null, null, single, null, 0, false, null, operator.ordinal());
+        return new Ast(AstTypes.UnaryOperation, null, null, single, null, 0, false, null,
+                operator.ordinal(), new ArrayList<>());
     }
     public static Ast binary(Ast left, Ast right, BinaryOperators operator) {
-        return new Ast(AstTypes.BinaryOperation, left, right, null, null, 0, false, null, operator.ordinal());
+        return new Ast(AstTypes.BinaryOperation, left, right, null, null, 0, false, null,
+                operator.ordinal(), new ArrayList<>());
     }
     public static Ast ternary(Ast left, Ast middle, Ast right, TernaryOperators operator) {
-        return new Ast(AstTypes.TernaryOperation, left, right, middle, null, 0, false, null, operator.ordinal());
+        return new Ast(AstTypes.TernaryOperation, left, right, middle, null, 0, false, null,
+                operator.ordinal(), new ArrayList<>());
     }
 
     public AstTypes getType() {
@@ -77,8 +92,12 @@ public class Ast {
     public boolean getBoolValue() {
         return boolValue;
     }
+
     public String getVarName() {
         return variableName;
+    }
+    public List<Ast> getChildren() {
+        return children;
     }
 
     public BinaryOperators getBinaryOp() {
