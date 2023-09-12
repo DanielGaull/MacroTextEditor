b667405152409908718d56bf0428938e6b7bd814
Daniel Gaull <danieljgaull@gmail.com>
2023-09-12 12:51:52 -0400

    Partially implemented parsing key binds

--- TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/modes/ModeParser.java
+++ TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/modes/ModeParser.java
@@ -7,6 +7,8 @@ import com.danieljgaull.texteditor.texteditor.data.VariableData;
 import com.danieljgaull.texteditor.texteditor.expressions.Ast;
 import com.danieljgaull.texteditor.texteditor.expressions.ExpressionEvaluator;
 import com.danieljgaull.texteditor.texteditor.expressions.ExpressionParser;
+import com.danieljgaull.texteditor.texteditor.instruction.Instruction;
+import com.danieljgaull.texteditor.texteditor.instruction.InstructionParser;
 import com.danieljgaull.texteditor.texteditor.util.StringUtils;
 
 import java.util.ArrayList;
@@ -16,10 +18,12 @@ public class ModeParser {
 
     private ExpressionParser expressionParser;
     private ExpressionEvaluator expressionEvaluator;
+    private InstructionParser instructionParser;
 
     public ModeParser() {
         expressionParser = new ExpressionParser();
         expressionEvaluator = new ExpressionEvaluator();
+        instructionParser = new InstructionParser();
     }
 
     // Should include all the way from the "mode" statement to the "endmode" statement
@@ -36,14 +40,22 @@ public class ModeParser {
         String suffix = "";
         // Set to 1 to skip header line
         for (int i = 1; i < lines.length; i++) {
-            String line = lines[i];
-            if (line.trim().length() > 0) {
+            String line = lines[i].trim();
+            if (line.length() > 0) {
                 // If the line starts with 'var', it's a variable
                 // 'bind' -> Key bind
                 // 'prefix'/'suffix' sets those expressions
                 if (line.startsWith("bind")) {
-                    // TODO: Parse key bind
                     // Need to find the "endbind" statement and get all those lines to send to parseKeyBind
+                    List<String> bindLines = new ArrayList<>();
+                    while (!lines[i].trim().equals("endbind")) {
+                        bindLines.add(lines[i]);
+                        i++;
+                        if (i >= lines.length) {
+                            throw new IllegalArgumentException("'bind' statement is missing the matching 'endbind' statement");
+                        }
+                    }
+                    keyBinds.add(parseKeyBind(bindLines));
                 } else if (line.startsWith("var")) {
                     variables.add(parseVar(line));
                 } else if (line.startsWith("prefix")) {
@@ -78,7 +90,19 @@ public class ModeParser {
         return new ModeVariable(new Variable(varName, type), defaultValue);
     }
     private KeyBind parseKeyBind(List<String> lines) {
-        return null;
+        String header = lines.get(0);
+        List<KeyBindCodes> keyCode = new ArrayList<>();
+        // TODO: Get the key combination from the header
+
+        List<Instruction> instructions = new ArrayList<>();
+        for (int i = 1; i < lines.size(); i++) {
+            String line = lines.get(i).trim();
+            if (line.length() > 0 && !line.equals("endbind")) {
+                instructions.add(instructionParser.parse(line));
+            }
+        }
+
+        return new KeyBind(keyCode, instructions);
     }
 
 }
