0b9b6783ba57bcbe01dec48590cededeb7d46a97
Daniel Gaull <danieljgaull@gmail.com>
2023-09-13 11:46:48 -0400

    Cleanup test code

--- TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/MainApplication.java
+++ TextEditor/src/main/java/com/danieljgaull/texteditor/texteditor/MainApplication.java
@@ -1,11 +1,5 @@
 package com.danieljgaull.texteditor.texteditor;
 
-import com.danieljgaull.texteditor.texteditor.expressions.Ast;
-import com.danieljgaull.texteditor.texteditor.expressions.ExpressionParser;
-import com.danieljgaull.texteditor.texteditor.macro.Macro;
-import com.danieljgaull.texteditor.texteditor.macro.MacroParser;
-import com.danieljgaull.texteditor.texteditor.modes.Mode;
-import com.danieljgaull.texteditor.texteditor.modes.ModeParser;
 import com.danieljgaull.texteditor.texteditor.util.PrimaryStageAware;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -35,30 +29,6 @@ public class MainApplication extends Application {
         stage.setTitle("Text Editor");
         stage.setScene(scene);
         stage.show();
-
-        ModeParser parser = new ModeParser();
-        String input = """
-                mode bullet
-                var tab_amount number (7 + 7)
-                prefix ("hello " + tab_amount)
-                suffix 5
-                
-                bind TAB
-                set tab_amount (tab_amount + 1)
-                endbind
-                
-                bind SHIFT+TAB
-                set tab_amount (tab_amount - 1)
-                endbind
-                
-                bind START+BACKSPACE
-                mode Default
-                endbind
-                
-                endmode
-                """;
-        Mode out = parser.parse(input);
-        System.out.println();
     }
 
     public static void main(String[] args) {
